2e6a5ae49dd6d2dc69cbad8a6234ab7eefa11eb4
Adi <Adi@maa>
2018-04-04 01:03:04 +0530

    Working with sending message

--- automationPractice/src/main/java/com/atmecs/automationPractice/MessagePage.java
+++ automationPractice/src/main/java/com/atmecs/automationPractice/MessagePage.java
@@ -3,13 +3,14 @@ package com.atmecs.automationPractice;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class MessagePage extends BasePage{
 
 	MessagePage(WebDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver,this);
 	}
 
 	@FindBy(css="div#contact-link a")
@@ -21,19 +22,38 @@ public class MessagePage extends BasePage{
 	@FindBy(css="select[name=id_order]")
 	private WebElement orderReference;
 	
-	@FindBy(css="span.filename")
+	@FindBy(css="input[name=fileUpload]")
 	private WebElement filePath;
 	
 	@FindBy(css="textarea#message")
 	private WebElement textBody;
 	
-	Select headline = new Select(selectHeadline);
-	Select reference = new Select(orderReference);
+	@FindBy(css="button#submitMessage")
+	private WebElement sendMessageButton;
+	
+	@FindBy(css="p.alert")
+	private WebElement successMessage;
+	
+	Select headline ;
+	Select reference;
 	
 	protected void composeMessage(String path,String message){
+		headline = new Select(selectHeadline);
+		reference = new Select(orderReference);
+		
 		headline.selectByIndex(1);
 		reference.selectByIndex(0);
 		filePath.sendKeys(path);
 		textBody.sendKeys(message);
+	}
+	
+	protected void sendMessage(String path,String message){
+		contactUsLInk.click();
+		composeMessage(path, message);
+		sendMessageButton.click();
+	}
+	
+	protected boolean messageSentSuccessful() {
+		return successMessage.getText().contains("successfully");		
 	}
 }
--- automationPractice/src/test/java/com/atmecs/automationPractice/MessageTest.java
+++ automationPractice/src/test/java/com/atmecs/automationPractice/MessageTest.java
@@ -4,9 +4,24 @@ import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.Test;
 
-public class MessageTest {
+public class MessageTest extends BaseTest{
+	
+	MessagePage messagePage;
+	
   @Test
-  public void f() {
-	  assertTrue(true);
+  public void composeMessage() {
+	String pathOfFile = properties.getProperty("pathOfFile");
+	String message = properties.getProperty("message");
+	String email = properties.getProperty("email");
+	String password = properties.getProperty("password");	
+	String url = properties.getProperty("url");
+		  
+	 LoginPage loginPage = new LoginPage(driver);
+	  loginPage.getUrl(url);
+	  loginPage.performLogin(email, password);
+	 
+	  messagePage = new MessagePage(driver);
+	messagePage.sendMessage(pathOfFile, message);
+	assertTrue(messagePage.messageSentSuccessful());
   }
 }
